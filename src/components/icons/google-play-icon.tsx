18ecb779a84ecfa7a171ffc711d29097300b2b81
Icaro <icarornunes@gmail.com>
2025-09-03 17:22:41 -0300

    alterado google-play-icon

--- src/components/icons/google-play-icon.tsx
+++ src/components/icons/google-play-icon.tsx
@@ -1,28 +1,7 @@
 import type { SVGProps } from 'react';
 
-export function GooglePlayIcon(props: SVGProps<SVGSVGElement>) {
+export function GooglePlayIcon({ className }: { className?: string }) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" {...props}>
-      <path
-        fill="#41b493"
-        d="M60.1,5.3C55.4,8.1,52,13.2,52,19.2v473.6c0,6,3.4,11.1,8.1,13.9L283,256,60.1,5.3z"
-      />
-      <path
-        fill="#f3f3f3"
-        d="m455.9,233.1-173-99.8-171,99.8L283,256l172.9-22.9z"
-      />
-      <path
-        fill="#f3f3f3"
-        d="m283,256l-171,99.8,171,99.8,172.9-99.8-172.9-99.8z"
-      />
-      <path
-        fill="#31846c"
-        d="M455.9,233.1,364.5,183,283,256l81.5,27,91.4-50z"
-      />
-      <path
-        fill="#55c9a7"
-        d="m455.9,278.9-91.4-50-81.5,27,81.5,27,91.4-4z"
-      />
-    </svg>
+      <img src="/google_play.webp" alt="Google Play Icon" className={className} />
   );
 }
