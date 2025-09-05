3481f31d054c864c6a33e539f737b53f36efda82
Icaro <icarornunes@gmail.com>
2025-09-05 13:32:18 -0300

    fix: enable assetPrefix in next.config and update predeploy script

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   output: "export",
   basePath: isProd ? "/popcorn-web" : "",
-  // assetPrefix: isProd ? "/popcorn-web/" : "",
+  assetPrefix: isProd ? "/popcorn-web/" : "",
   trailingSlash: true,
   /* config options here */
   typescript: {
