6a090a2ee595c0cb890cbbee19bd9ae5da11019a
Icaro <icarornunes@gmail.com>
2025-09-06 19:52:36 -0300

    agora vai

--- src/components/icons/popcorn-icon.tsx
+++ src/components/icons/popcorn-icon.tsx
@@ -3,7 +3,7 @@ export function PopcornIcon({ className }: { className?: string }) {
   console.log('process.env.PUBLIC_URL :>> ', process.env.PUBLIC_URL);
   return (
     <div>
-      <Image src={`${process.env.PUBLIC_URL}/icon_visto.png`} alt="Popcorn Icon" width={50} height={50} className={className} />
+      <Image src={`${process.env.PUBLIC_URL ? `${process.env.PUBLIC_URL}/icon_visto.png` : '/icon_visto.png'}`} alt="Popcorn Icon" width={50} height={50} className={className} />
     </div>
   );
 }
