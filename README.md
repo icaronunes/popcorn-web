ae6611480121e720800b1e7baad5bde75414bf52
Icaro <icarornunes@gmail.com>
2025-09-06 20:18:46 -0300

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-Landing page para divulgar o PopCorn Show - https://play.google.com/store/apps/details?id=br.com.icaro.filme
--- src/components/icons/google-play-icon.tsx
+++
@@ -1,7 +0,0 @@
-import Image from 'next/image';
-
-export function GooglePlayIcon({ className }: { className?: string }) {
-  return (
-      <Image src={`${process.env.NODE_ENV === 'production' ? `popcorn-web/google_play.webp` : '/google_play.webp'}`} alt="Google Play Icon" className={className} height={50} width={200} />
-  );
-}
