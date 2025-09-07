0d5924bf284597bb4258ac5e9945899cda1df623
Icaro <icarornunes@gmail.com>
2025-09-07 19:39:24 -0300

    vercel deploy

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,10 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: isProd ? "/popcorn-web" : '',
-  assetPrefix: isProd ? "/popcorn-web" : '',
-  distDir: 'out',
+  // output: "export",
+  // basePath: isProd ? "/popcorn-web" : '',
+  // assetPrefix: isProd ? "/popcorn-web" : '',
+  // distDir: 'out',
   trailingSlash: true,
   // trailingSlash: true,
   /* config options here */
